4b60123ec561fa5b27c7e605226d1631861a52a7
Alexandr <Sashob84@gmail.com>
2018-10-18 15:08:38 +0300

    Initial Commit

---
+++ Jetpal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jetpal
+//
+//  Created by Alexandr on 10/18/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
