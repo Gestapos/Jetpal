735b14dd28d8c03365b2cdd06108c803a3ab43f4
Alexandr <Sashob84@gmail.com>
2018-10-19 17:10:05 +0300

    add LoginVC

---
+++ Jetpal/Controller/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Jetpal
+//
+//  Created by Alexandr on 10/19/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Jetpal/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Jetpal
+//
+//  Created by Alexandr on 10/19/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import Foundation
+
+//MARK: - Segues
+let TO_LOGIN = "toLogin"
