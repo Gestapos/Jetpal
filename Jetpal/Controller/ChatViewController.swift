1f321da0db8a88f61f3051c68d822cee3ff25c0d
Alexandr <Sashob84@gmail.com>
2018-10-24 20:03:57 +0300

    some issues

--- Jetpal/Controller/ChatViewController.swift
+++ Jetpal/Controller/ChatViewController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class ChatViewController: UIViewController {
+    
     @IBOutlet weak var messageTxtBox: UITextField!
-    
     @IBOutlet weak var menuButton: UIButton!
-    
     @IBOutlet weak var channelNameLabel: UILabel!
+    
+    var isKeyboardAppear = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
-        
+        setupGestures()
+        notificationSetup()
         
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail { (success) in
@@ -33,6 +29,44 @@ class ChatViewController: UIViewController {
         }
     }
     
+    
+    //MARK: - Gestures
+    func setupGestures() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatViewController.handleTap))
+        view.addGestureRecognizer(tap)
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+    //MARK: - Notifications
+    func notificationSetup() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
+    }
+    
+    //MARK: - Keyboard hide and change methods
+    @objc func keyboardWillHide() {
+        self.view.frame.origin.y = 0
+    }
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if messageTxtBox.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
+    //MARK: - Notification Methods
     @objc func userDataDidChange(_ notif: Notification) {
         if AuthService.instance.isLoggedIn {
             onLoginGetMessages()
@@ -45,14 +79,26 @@ class ChatViewController: UIViewController {
         updateWithChannel()
     }
     
+    //MARK: - Channel and Messages methods
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLabel.text = "#\(channelName)"
         getMessages()
     }
     
+    //MARK: - SendMessages, GetMessages
     @IBAction func sendMessagePressed(_ sender: Any) {
-        
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+            guard let message = messageTxtBox.text else {return}
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success {
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                }
+            }
+        }
     }
     
     func onLoginGetMessages() {
